e0f1884c8d6789cab843064a02d486f2cb9af995
Pravin B Shelar <pshelar@ovn.org>
2016-08-02 00:25:23 -0700

    datapath: compat: fix SKB_INIT_FILL_METADATA_DST definition
    In case of OVS using compat fill metadata dst implementation we need to
    setup temperory dst.
    
    Signed-off-by: Pravin B Shelar <pshelar@ovn.org>

--- datapath/linux/compat/gso.h
+++ datapath/linux/compat/gso.h
@@ -166,7 +166,7 @@ static inline void ovs_dst_release(struct dst_entry *dst)
 #define ovs_dst_release dst_release
 #endif
 
-#ifndef HAVE_NDO_FILL_METADATA_DST
+#ifndef USE_UPSTREAM_TUNNEL
 #define SKB_INIT_FILL_METADATA_DST(skb)	OVS_GSO_CB(skb)->fill_md_dst = NULL;
 
 #define SKB_RESTORE_FILL_METADATA_DST(skb)	do {			\
